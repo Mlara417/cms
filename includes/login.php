98bcce1952da6bd826a1122022b5274c89e4f882
Moses Lara <mlara417@gmail.com>
2019-06-24 12:26:24 -0500

    changed firstname,lastname session variable

--- includes/login.php
+++ includes/login.php
@@ -35,8 +35,8 @@ if(isset($_POST['login'])) {
         if ($username === $db_username && $password === $db_password){
             
             $_SESSION['username'] = $db_username;
-            $_SESSION['firstname'] = $db_firstname;
-            $_SESSION['lastname'] = $db_lastname;
+            $_SESSION['user_firstname'] = $db_firstname;
+            $_SESSION['user_lastname'] = $db_lastname;
             $_SESSION['user_role'] = $db_role;
             
             header("Location: ../admin/index.php");
