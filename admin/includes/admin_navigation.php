883eedc36b640e00920e2ca05aa1cf41a11c9898
Moses Lara <mlara417@gmail.com>
2019-07-04 11:42:02 -0500

    Added a view post link & added a warning that comment fields can't be left empty

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -17,7 +17,7 @@
             <ul class="nav navbar-right top-nav">
                <li><a href="../index.php">Home</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['user_firstname'] . " " . $_SESSION['user_lastname'] ?>  <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['username']; ?>  <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
--- post.php
+++ post.php
@@ -61,12 +61,17 @@
 <?php
 
 if(isset($_POST['create_comment'])){
+    
     $the_post_id = $_GET['p_id'];
     
     $comment_author = $_POST['comment_author'];
     $comment_email = $_POST['comment_email'];
     $comment_content = $_POST['comment_content'];
+
     
+    if(!empty($comment_author) && !empty($comment_email) && !empty($comment_content)){
+        
+        
     $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
     
     $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved', now())";
@@ -83,9 +88,14 @@ if(isset($_POST['create_comment'])){
     $query .= "WHERE post_id = $the_post_id ";
     
     $update_comment_count = mysqli_query($connection, $query);
-    
-    
-    
+        
+        
+        
+    } else {
+        
+        echo "<script>alert('Fields cannot be empty')</script>";
+        
+    }
     
 }
 
