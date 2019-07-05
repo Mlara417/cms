4ddfccb6d062c563e2209ed756f32e70c45eaf6b
Moses Lara <mlara417@gmail.com>
2019-07-05 09:35:11 -0500

    commented out old comment count query

--- post.php
+++ post.php
@@ -84,10 +84,10 @@ if(isset($_POST['create_comment'])){
     
     
     
-    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+/*    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
     $query .= "WHERE post_id = $the_post_id ";
     
-    $update_comment_count = mysqli_query($connection, $query);
+    $update_comment_count = mysqli_query($connection, $query);*/
         
         
         
